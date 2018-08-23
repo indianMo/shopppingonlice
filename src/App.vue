4afb8395131e1d6afb2e262d270862ac35dd7988
indianMo <indianmo@163.com>
2018-08-23 11:51:03 +0800

    实现顶部效果

--- src/App.vue
+++ src/App.vue
@@ -116,11 +116,43 @@
 </template>
 
 <script>
+import $ from "jquery";
+
 export default {
-  name: "app"
+  name: "container"
 };
+
+$(document).ready(function() {
+  $("#menu2 li a").wrapInner('<span class="out"></span>');
+  $("#menu2 li a").each(function() {
+    $('<span class="over">' + $(this).text() + "</span>").appendTo(this);
+  });
+
+  $("#menu2 li a").hover(
+    function() {
+      $(".out", this)
+        .stop()
+        .animate({ top: "48px" }, 300); // move down - hide
+      $(".over", this)
+        .stop()
+        .animate({ top: "0px" }, 300); // move down - show
+    },
+    function() {
+      $(".out", this)
+        .stop()
+        .animate({ top: "0px" }, 300); // move up - show
+      $(".over", this)
+        .stop()
+        .animate({ top: "-48px" }, 300); // move up - hide
+    }
+  );
+});
 </script>
 
 <style>
-  @import url("./assets/site/css/style.css");
+  @import url("./assets/static/site/css/style.css");
+  @import url("./assets/lib/css/style.css"); 
+  #menu2 {
+    background-image: none;
+  }
 </style>
