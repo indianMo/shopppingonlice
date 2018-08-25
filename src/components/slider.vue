8858464731b1aa305cf03c52be6cee37d000d047
indianMo <indianmo@163.com>
2018-08-25 09:04:24 +0800

    首页和轮播图的完成

---
+++ src/components/slider.vue
@@ -0,0 +1,23 @@
+<template>
+    <el-carousel>
+        <el-carousel-item v-for="item in sliderlist" :key="item.id">
+            <a href="javascript:void(0)">
+                <img :src="item.img_url" alt=""></a>
+        </el-carousel-item>
+    </el-carousel>
+</template>
+<script>
+export default {
+    props:["sliderlist"],
+    data: function () {
+        return {
+            
+        }
+    },
+    methods:{ }
+}    
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,35 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Index from './components/index.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter);
+Vue.use(ElementUI);
+
+const routes = [
+  {
+    path: '/',
+    redirect: "/index"
+  },
+  {
+    path: "/index",
+    component: Index
+  },
+  {
+    path: "/index",
+    component: Index
+  }
+]
+
+let router = new VueRouter({
+  routes
+})
+
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
